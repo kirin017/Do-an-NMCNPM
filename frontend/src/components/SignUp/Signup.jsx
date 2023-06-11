d5f242e77412c9bfe6bb0af110d028e1cdc1dc8a
CapHaTri <trihx2003@gmail.com>
2023-06-11 23:52:49 +0700

    Update SignUp

--- frontend/src/components/SignUp/Signup.jsx
+++ frontend/src/components/SignUp/Signup.jsx
@@ -5,9 +5,6 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
@@ -26,91 +23,130 @@ export default function SignUp() {
   };
 
   return (
+    
       <Container component="main" maxWidth="xs">
         <CssBaseline />
+        
+        <Box
+         style = {{
+           border: '1px solid rgba(0, 0, 0, 0.12)',
+           boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.25)',
+           width: '1000px',
+           height: '750px',
+           marginLeft: '-270px',
+         }}
+         >
         <Box
           sx={{
-            marginTop: 8,
+            marginTop: 120,
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar 
+            style={{ color: 'white', backgroundColor: '#0099FF'}}
+            sx={{ m: 1, bgcolor: 'secondary.main' }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        autoComplete="given-name"
+                        name="firstName"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="family-name"
+                      />
+                   </Grid>
+                  </Grid>
+                    <Grid 
+                      style={{ marginTop: '20px'}}
+                      item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                        />
+                    </Grid>
+                    <Grid 
+                      style={{ marginTop: '20px'}}
+                      item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="phone"
+                          label="Phone Number"
+                          name="phone"
+                          autoComplete="phone"
+                        />
+                    </Grid>
+                    <Grid 
+                      style={{ marginTop: '20px'}}
+                      item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                        />
+                    </Grid>
+                    <Grid 
+                      style={{ marginTop: '20px'}}
+                      item xs={12}>
+                        <FormControlLabel
+                          control={<Checkbox value="allowExtraEmails" color="primary" />}
+                          label="I want to receive inspiration, 
+                          marketing promotions and updates via email."
+                        />
+                    </Grid>
+
+                    <Button
+                    style={{ marginTop: '50px', color: 'white', backgroundColor: '#0099FF'}}
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                    >
+                    Sign Up
+                    </Button>
+                    <Grid container>
+                    <Grid item
+                      style={{ marginTop: '20px'}}
+                    >
+                        <Link href="/login" variant="body2">
+                        {"Already have an account? Log in"}
+                        </Link>
+                    </Grid>
+                    </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
+        <Copyright 
+          style={{ marginTop: '40px'}}
+          sx={{ mt: 5 }} />
+        </Box>
       </Container>
   );
 }
