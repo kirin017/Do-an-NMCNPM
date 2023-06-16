70a3674c60ffbc7aa4a3664187209496a52432e1
CapHaTri <trihx2003@gmail.com>
2023-06-16 16:47:40 +0700

    update...

--- backend/src/controllers/homeController.js
+++ backend/src/controllers/homeController.js
@@ -1,9 +1,10 @@
 import db from '../models/index'
+import CRUDservice from '../services/CRUDservice';
 
 let getHomePage = async (req, res) => {
   
     try {
-        let data = await db.User.findAll();
+        let data = await db.User.findOne({where : {username : '2'}});
         return res.render("homepage.ejs", {
         data: JSON.stringify(data),
         });
@@ -12,9 +13,17 @@ let getHomePage = async (req, res) => {
       }
     };  
    
+let getCRUD = (req, res) =>{
+  return res.render("crud.ejs");
+}
 
-
+let postCRUD = async (req, res) =>{
+  await CRUDservice.createNewUser(req.body)
+  return res.send('post crud from server')
+}
 
 module.exports = {
     getHomePage: getHomePage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
 }
---
+++ backend/src/services/CRUDservice.js
@@ -0,0 +1,39 @@
+import bcrypt from "bcryptjs";
+import db from "../models/index"
+const salt = bcrypt.genSaltSync(10);
+let createNewUser = async (data) =>{
+    let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+    return new Promise(async (resolve, reject) => {
+    try {
+      let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+      await db.User.create({
+        name: data.name,
+        username: data.username,
+        password: hashPasswordFromBcrypt,
+        phoneNumber: data.phoneNumber,
+        email: data.email,
+        gender:
+          data.gender == 1 ? "female" : data.gender == 0 ? "male" : "other",
+        typeUser: data.typeUser,
+      });
+      resolve("create new user succeeded");
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+}
+let hashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+          let hashPassword = await bcrypt.hashSync(password, salt);
+          resolve(hashPassword);
+        } catch (e) {
+          reject(e);
+        }
+      });
+
+};
+module.exports = {
+    createNewUser : createNewUser
+}
