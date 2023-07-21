d26d11d14c00023916c7a943ccf431bf09e2ee81
Halo <21521921@gm.uit.edu.vn>
2023-07-22 05:25:08 +0700

    Hoàn thiện chức năng của About Us và Liên hệ

--- frontend/src/components/AboutUs/AboutUs.jsx
+++ frontend/src/components/AboutUs/AboutUs.jsx
@@ -4,28 +4,69 @@ import './AboutUs.css';
 
 class AboutUs extends React.Component {
   render() {
-    const phoneNumber = "0941 218 057";
     const adminTeam = ["Đào Nhật Khánh", "Cáp Hữu Anh Trí", "Nguyễn Hữu Huy", "Nguyễn Thành Đăng"];
-    const email = "trihx2003@gmail.com";
+    const partners = [
+      { name: "Nike", website: "https://www.nike.com/vn/" },
+      { name: "Shopee", website: "https://shopee.vn/" },
+      { name: "Microsoft", website: "https://www.microsoft.com/vi-vn/" }
+    ];
+    const jobOpenings = [
+      { title: "Sports Writer", description: "We are looking for talented sports writers to join our team. If you have a passion for sports and a flair for writing, we'd love to hear from you!" },
+      { title: "Web Developer", description: "Are you a skilled web developer with a passion for sports? Join us in enhancing our website and creating innovative features for our users." },
+      { title: "Social Media Specialist", description: "Do you have a knack for engaging content and a love for sports? Help us grow our social media presence and connect with our community." }
+    ];
+    const privacyPolicy = "Link to your privacy policy page or content.";
+    const termsOfUse = "Link to your terms of use page or content.";
 
     return (
-      <Grid container justify="center" className="about-us">
-        <div className="card">
-          <h1>About Us</h1>
-          <div className="image-background"></div>
-          <p>Welcome to our sports website! We are passionate about sports and strive to bring you the latest news, updates, and insights from the world of sports.</p>
-          <p>Our team of dedicated writers and enthusiasts cover a wide range of sports, including football, basketball, tennis, and more. We aim to provide comprehensive coverage and in-depth analysis to keep you informed and engaged.</p>
-          <p>Whether you're a die-hard sports fan or a casual observer, we've got something for everyone. Stay tuned for exciting content and join us on this thrilling journey through the world of sports.</p>
-          <h2>Contact Information</h2>
-          <p><strong>Phone:</strong> {phoneNumber}</p>
-          <h3>ADMIN Team</h3>
-          <ul>
-            {adminTeam.map((admin, index) => (
-              <li key={index}>{admin}</li>
-            ))}
-          </ul>
-          <p><strong>Email:</strong> {email}</p>
-        </div>
+      <Grid container className="about-us">
+        <Grid item md={3} className="left-column">
+          <div className="image-left"></div>
+        </Grid>
+        <Grid item md={6} className="middle-column">
+          <div className="image-middle"></div>
+          <div className="card">
+            <div className="content">
+              <h1>About Us</h1>
+              <h2>History</h2>
+              <p>
+              We started the project in 2021 with the goal of bringing the community the best quality sports information. Since our founding, we have constantly grown and built a strong community of sports enthusiasts. Up to now, we have won many prestigious awards in the sports field and are constantly improving to bring the best user experience.
+              </p>
+              <h2>Our Team</h2>
+              <ul>
+                {adminTeam.map((admin, index) => (
+                  <li key={index}>{admin}</li>
+                ))}
+              </ul>
+              <h2>Partners and Sponsors</h2>
+              <ul>
+                {partners.map((partner, index) => (
+                  <li key={index}><a href={partner.website} target="_blank" rel="noopener noreferrer">{partner.name}</a></li>
+                ))}
+              </ul>
+              <h2>Recruitment</h2>
+              <ul>
+                {jobOpenings.map((job, index) => (
+                  <li key={index}>
+                    <h3>{job.title}</h3>
+                    <p>{job.description}</p>
+                  </li>
+                ))}
+              </ul>
+              <h2>Policy And Terms</h2>
+              <p>
+                <strong>Privacy Policy:</strong> <a href={privacyPolicy} target="_blank" rel="noopener noreferrer">Link to Privacy Policy</a>
+              </p>
+              <p>
+                <strong>Terms of Use:</strong> <a href={termsOfUse} target="_blank" rel="noopener noreferrer">Link to Terms of Use</a>
+              </p>
+              
+            </div>
+          </div>
+        </Grid>
+        <Grid item md={3} className="right-column">
+          <div className="image-right"></div>
+        </Grid>
       </Grid>
     );
   }
--- frontend/src/components/Contact/contact.jsx
+++ frontend/src/components/Contact/contact.jsx
@@ -1,30 +1,42 @@
-
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import './contact.css';
 
 class Contact extends React.Component {
-    render() {
-        const phoneNumber = "0941218057";
-        const adminTeam = ["Đào Nhật Khánh", "Cáp Hữu Anh Trí", "Nguyễn Hữu Huy", "Nguyễn Thành Đăng"];
-        const email = "trihx2003@gmail.com";
-    
-        return (
-            <Grid style = {{marginTop : '200px', marginLeft : '50px'}}>
-          <div >
+  render() {
+    const phoneNumber = "0941218057";
+    const adminTeam = ["Đào Nhật Khánh", "Cáp Hữu Anh Trí", "Nguyễn Hữu Huy", "Nguyễn Thành Đăng"];
+    const email = "trihx2003@gmail.com";
+
+    return (
+      <Grid container justify="center" className="contact-container">
+        <div className="contact-card">
+          <h1>Thông tin liên hệ</h1>
+          <p><strong>Số điện thoại:</strong> {phoneNumber}</p>
+          <h2>Đội ngũ ADMIN</h2>
+          <ul>
+            {adminTeam.map((admin, index) => (
+              <li key={index}>{admin}</li>
+            ))}
+          </ul>
+          <p><strong>Email:</strong> {email}</p>
+          <h2>Kết nối với chúng tôi qua mạng xã hội</h2>
+          <ul className="social-media-links">
+            <li>
+              <a href="https://www.facebook.com/MiamiHeat" target="_blank" rel="noopener noreferrer">Facebook</a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@mazuong2k" target="_blank" rel="noopener noreferrer">Youtube</a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/miamiheat/?hl=en" target="_blank" rel="noopener noreferrer">Instagram</a>
+            </li>
             
-            <h1>Thông tin liên hệ</h1>
-            <p><strong>Số điện thoại:</strong> {phoneNumber}</p>
-            <h2>Đội ngũ ADMIN</h2>
-            <ul>
-              {adminTeam.map((admin, index) => (
-                <li key={index}>{admin}</li>
-              ))}
-            </ul>
-            <p><strong>Email:</strong> {email}</p>
-          </div>
-          </Grid>
-        );
-      }
+          </ul>
+        </div>
+      </Grid>
+    );
+  }
 }
 
 export default Contact;
